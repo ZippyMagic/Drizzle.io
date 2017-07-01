5ccdb2a45e04bc0941bd1c51b28f38f776cb86d9
ZippyMagic <zippymagician1@gmail.com>
2017-07-01 15:12:49 -0400

    Update compileBuild-1-0-0.js

--- src/compileBuild-1-0-0.js
+++ src/compileBuild-1-0-0.js
@@ -13,5 +13,27 @@ function use(Import) {
         FileSystem = new FileSystem();
     } else {
         return console.log("net.Import.Module: {" + Import + "} does not exist.")
-	}
+    }
+}
+function FileSystem() {
+  this.writeTextFile = function(  name, path, contents ) {
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+    var a = fso.CreateTextFile(path + name + ".txt", true);
+    var letter = 0;
+    var letter2;
+    var string = "";
+    var string2;
+    for(I = 0; I < contents.length; I++) {    
+      if(contents.charAt[letter] === "/n" && contents.charAt[letter + 1] === "n") {
+        letter2 = letter;
+        letter = letter2 + 2;
+        a.WriteLine(string)      
+      } else {
+        string2 = string;
+        string = string2 + contents.charAt[letter];
+        letter++
+      }
+    }
+    a.Close();
+  }
 }
