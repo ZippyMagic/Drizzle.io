0e9da53cd50f71dd9cac29b9174b0d8dac15387d
ZippyMagic <zippymagician1@gmail.com>
2017-07-01 14:46:15 -0400

    Update system.js

--- src/api/system.js
+++ src/api/system.js
@@ -5,18 +5,18 @@
 */
 
 function System() {
-		
-	this.Print = function(text) {
-		var body = document.body.innerHTML;
-			
-		document.body.innerHTML = body + text + '<br>';
-	}
-    this.getDate = function() {    	
-		var today = new Date();
-        var date = today.getFullYear() + '/' + today.getMonth() + '/' + today.getDay();
-		var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-		var Date2 = date + ' -- ' + time;
+    
+    this.Print = function(text) {
+        var body = document.body.innerHTML;
         
-		return Date2;
+        document.body.innerHTML = body + text + "<br>";
+    }
+    this.getDate() = function() {
+        var today = new Date();
+        var date = today.getFullYear() + '/' + today.getMonth() + '/' + today.getDay();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var Date2 = date + ' -- ' + time;
+        
+        return Date2;
     }
 }
