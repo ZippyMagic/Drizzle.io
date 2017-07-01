844d0073da932a7ee6bfef621f017af95974538d
ZippyMagic <zippymagician1@gmail.com>
2017-07-01 15:11:05 -0400

    Update compileBuild-1-0-0.js

--- src/compileBuild-1-0-0.js
+++ src/compileBuild-1-0-0.js
@@ -1,1 +1,17 @@
-'use-strict'
+var System;
+var FileSystem;
+function use(Import) {	
+    if(Import === "System") {
+        System = new System();
+    } else if(Import.charAt(0) === "C" && Import.charAt(1) === ":") {
+        readTextFile(Import + "\project.json", function(text){
+            var data = JSON.parse(text);
+            var key = data.module-name;
+            window[key] = new Object;
+		    });
+    } else if(Import === "FileSystem") {
+        FileSystem = new FileSystem();
+    } else {
+        return console.log("net.Import.Module: {" + Import + "} does not exist.")
+	}
+}
