eba56b23ac05c1a7e012b00e6f23d02f98e29f12
ZippyMagic <zippymagician1@gmail.com>
2017-07-02 09:03:01 -0400

    Update lockFile.js

--- test/api/lockFile.js
+++ test/api/lockFile.js
@@ -1,16 +1,12 @@
 //
-// Lock File Test API
+// openFileTerminal Test API
 // NOTE: Would be part of {FileSystem} API if added
 // @ZippyMagician
 //
 
 function apiLockFile() {
-    this.lockFile = function(err) {
+    this.openFileTerminal = function(err) {
         <%#lockFile.Run.RunC()%> ;
-        window.onerror = err(error);
-    }
-    this.unlockFile = function(err) {
-        <%#unlockFile.Run.RunC()%> ;
         window.onerror = err(error);
     }
 }
