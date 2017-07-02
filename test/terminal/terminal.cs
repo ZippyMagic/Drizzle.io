92d1894c28f24417a3731a8c3764fa4d49957691
ZippyMagic <zippymagician1@gmail.com>
2017-07-02 08:37:26 -0400

    Update terminal.cs

--- test/terminal/terminal.cs
+++ test/terminal/terminal.cs
@@ -1,10 +1,14 @@
 using System;
 
+public enum Commands
+{
+    1 = "exit",
+    2 = "help",
+}
 public class Terminal
 {
     public static void Main()
     {
-        
         Console.WriteLine("Accessing Terminal Code and scripts...");
     }
 }
